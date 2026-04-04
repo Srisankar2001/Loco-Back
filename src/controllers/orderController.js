ac027af0c0e75a60b0ddeac5201272f7e388cf2b
Nilushiya <nilushiyak@gmail.com>
2026-04-04 20:40:08 +0530

    order changed

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -601,6 +601,7 @@ export const getOrderForUser = async (req, res) => {
         { model: Station, as: "station" },
       ],
     });
+    console.log("Fetched order:", order);
     if (!order) {
       return res.status(400).json(clientErrorResponse("Order not found."));
     }
